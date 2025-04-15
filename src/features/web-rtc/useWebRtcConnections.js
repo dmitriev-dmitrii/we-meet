7d388e932a22aba7653d61986f677770ec13a5c0
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-04-15 20:20:20 +0300

    test

--- src/features/web-rtc/useWebRtcConnections.js
+++ src/features/web-rtc/useWebRtcConnections.js
@@ -14,6 +14,28 @@ import freeice from "freeice";
 
 const configuration = {
     iceServers: freeice()
+    // iceServers: [
+    //     // STUN от Google (базовый)
+    //     { urls: "stun:stun.l.google.com:19302" },
+    //
+    //     // Бесплатный TURN-сервер (резервный)
+    //     {
+    //         urls: "turn:openrelay.metered.ca:80",
+    //         username: "openrelayproject",
+    //         credential: "openrelayproject"
+    //     },
+    //     {
+    //         urls: "turn:openrelay.metered.ca:443",
+    //         username: "openrelayproject",
+    //         credential: "openrelayproject"
+    //     },
+    //     {
+    //         urls: "turn:openrelay.metered.ca:443?transport=tcp",
+    //         username: "openrelayproject",
+    //         credential: "openrelayproject"
+    //     }
+    // ],
+    // iceTransportPolicy: "relay" // Форсировать TURN, если не работает P2P
 };
 
 export const useWebRtcConnections = () => {
@@ -171,7 +193,7 @@ export const useWebRtcConnections = () => {
                 console.warn('updatePeerIceCandidate , remoteUserId is empty', remoteUserId)
             }
 
-            await peerConnections[remoteUserId].addIceCandidate(candidate);
+            await peerConnections[remoteUserId].addIceCandidate(new RTCIceCandidate(candidate));
         } catch (e) {
             console.error('updatePeerIceCandidate', e)
         }
